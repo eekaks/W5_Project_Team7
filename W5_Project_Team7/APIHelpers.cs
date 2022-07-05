fc3f9e4cbc90ba5555590f8d961276bcdc1ae52e
Eetu Laine <eetulaine@gmail.com>
2022-07-05 10:33:57 +0300

    apihelper class added

---
+++ W5_Project_Team7/APIHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Apitestailua
+{
+    public static class APIHelper
+    {
+        public static HttpClient GetHttpClient(string url)
+        {
+            var client = new HttpClient { BaseAddress = new Uri(url) };
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+}
