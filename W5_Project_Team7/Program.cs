f5f4f4fcdebb203792e1c543f5c73ba51ba8b5ed
Eetu Laine <eetulaine@gmail.com>
2022-07-05 10:55:20 +0300

    added V1Events.cs for event search functionality

--- W5_Project_Team7/Program.cs
+++ W5_Project_Team7/Program.cs
@@ -1,14 +1,43 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace W5_Project_Team7
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string url = "http://open-api.myhelsinki.fi/v1/events/";
 
-            Console.WriteLine("Tämä on Eetun branchi.");
+            try
+            {
+                using (var client = APIHelper.GetHttpClient(url))
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        string json = await response.Content.ReadAsStringAsync();
+
+
+                        V1Events results = JsonSerializer.Deserialize<V1Events>(json);
+
+                        foreach (var item in results.data)
+                        {
+                            Console.WriteLine("ID: " + item.id + " Name: " + item.name.fi);
+                        }
+
+                        Console.WriteLine($"Found {results.meta.count} items.");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
