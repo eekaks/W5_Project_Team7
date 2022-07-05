53be28df51d5967c3394b6c8033b2af8226b42ea
Tiia Meriranta <tiia.meriranta@gmail.com>
2022-07-05 09:42:12 +0300

    lisätyt kommentit

---
+++ W5_Project_Team7/Project_Class_Tiia.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace W5_Project_Team7
+{
+
+    class Project_Class_Tiia
+    {
+        //Tähän luokan fieldejä ja muuta kivaa
+    }
+}
