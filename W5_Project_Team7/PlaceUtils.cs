f42e27560371021f914351b8ca0adcd872d73f08
Eetu Laine <eetulaine@gmail.com>
2022-07-07 13:36:10 +0300

    refactored code into different class files

---
+++ W5_Project_Team7/PlaceUtils.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace W5_Project_Team7
+{
+    class PlaceUtils
+    {
+        public static bool CheckArea(V2Place helsinkiPlace)
+        {
+            string[] neighbourhoods = new[]
+            {
+                "Kluuvi", "Kamppi", "Kruununhaka", "Punavuori", "Ullanlinna", "Kallio", "Vallila", "Eira",
+                "Kaivopuisto", "Etu - Töölö", "Sörnäinen"
+            };
+            return neighbourhoods.Contains(helsinkiPlace.location.address.neighbourhood);
+        }
+    }
+}
